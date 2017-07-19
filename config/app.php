709b486ba65eefd410df073b431304844ed33516
= <=>
2017-07-19 11:40:33 +0100

    Configuring Laratrust

--- config/app.php
+++ config/app.php
@@ -177,6 +177,12 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         Third party service providers
+        */
+
+        Laratrust\LaratrustServiceProvider::class,
+
     ],
 
     /*
@@ -209,6 +215,7 @@ return [
         'Gate' => Illuminate\Support\Facades\Gate::class,
         'Hash' => Illuminate\Support\Facades\Hash::class,
         'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Laratrust' => Laratrust\LaratrustFacade::class,
         'Log' => Illuminate\Support\Facades\Log::class,
         'Mail' => Illuminate\Support\Facades\Mail::class,
         'Notification' => Illuminate\Support\Facades\Notification::class,
