27e2636254f42952cf8e573d20d3a326ca5dcfba
= <=>
2017-12-18 20:48:53 +0000

    Created a Database Schema for Blog Posts

--- resources/views/_includes/nav/manage.blade.php
+++ resources/views/_includes/nav/manage.blade.php
@@ -7,19 +7,20 @@
             <li><a href="{{route('manage.dashboard')}}" class="{{ Nav::isRoute('manage.dashboard') }}">Dashboard</a></li>
         </ul>
 
+
         <p class="menu-label">
-            Content
+            Content Post
         </p>
         <ul class="menu-list">
-            <li><a href="#" class="">Blog Posts</a></li>
-            {{-- <li>
-              <a class="has-submenu {{Nav::hasSegment(['roles', 'permissions'], 2)}}">Roles &amp; Permissions</a>
-              <ul class="submenu">
-                <li><a href="{{route('roles.index')}}" class="{{Nav::isResource('roles')}}">Roles</a></li>
-                <li><a href="{{route('permissions.index')}}" class="{{Nav::isResource('permissions')}}">Permissions</a></li>
-              </ul>
-            </li> --}}
-        </ul>
+            <li><a href="{{route('posts.index')}}" class="{{ Nav::isResource('posts', 2) }}">Blog Posts</a></li>
+            {{--<li>--}}
+                {{--<a class="has-submenu {{ Nav::hasSegment(['roles', 'permissions'], 2) }}">Roles &amp; Permissions</a>--}}
+                {{--<ul class="submenu">--}}
+                    {{--<li><a href="{{route('roles.index')}}" >Roles</a></li>--}}
+                    {{--<li><a href="{{route('permissions.index')}}" class="">Permissions</a></li>--}}
+                {{--</ul>--}}
+            {{--</li>--}}
+
 
         <p class="menu-label">
             Administration
